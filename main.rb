ae332162edecf7bd43b5a2b2c5ed4aed30eac516
ufeanei <akaranjoh@yahoo.com>
2015-11-21 23:43:43 +0100

    added styling to front page

--- main.rb
+++ main.rb
@@ -5,33 +5,41 @@ set :sessions, true
 
 POCKET = 1000
 
+
 get "/" do
-	erb:template
-	
+  erb:template
 end
 
 post "/name" do
-	session[:name] = params[:name]
-	redirect "/newbet"
+  if session[:name] == ""
+  	@error = "Please name can't be empty"
+  	redirect "/"
+  else
+    session[:name] = params[:name]
+    redirect "/newbet"
+  end
 end
 
 
 get "/newbet" do
-	 @your_pocket = POCKET
-	erb:bet	
+  @your_pocket = POCKET
+  erb:bet	
 end
 
 post "/bet" do
   
   if params[:bet].to_i > POCKET
+    redirect "/newbet"
   	@error = "Your bet must be lower than your pocket"
-  	redirect "/newbet"
+  elsif params[:bet] == ""
+  	 redirect "/newbet"	
+  	@error = "Please fill in your bet"
   else
     session[:bet] = params[:bet]
     redirect "/game"
   end
 end
  
- get "/game" do
- 	erb:game
- end
+get "/game" do
+  erb:game
+end
