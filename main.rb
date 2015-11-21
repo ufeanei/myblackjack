7d053cabdec202c5d40a1e5b421d134775665cf7
ufeanei <akaranjoh@yahoo.com>
2015-11-21 18:06:43 +0100

    added new bet view file, game view file, completed routes in main

--- main.rb
+++ main.rb
@@ -3,11 +3,35 @@ require 'sinatra'
 
 set :sessions, true
 
+POCKET = 1000
+
 get "/" do
 	erb:template
+	
+end
+
+post "/name" do
+	session[:name] = params[:name]
+	redirect "/newbet"
 end
 
 
-get "/home" do
-	"just show this on home"
+get "/newbet" do
+	 @your_pocket = POCKET
+	erb:bet	
 end
+
+post "/bet" do
+  
+  if params[:bet].to_i > POCKET
+  	@error = "Your bet must be lower than your pocket"
+  	redirect "/newbet"
+  else
+    session[:bet] = params[:bet]
+    redirect "/game"
+  end
+end
+ 
+ get "/game" do
+ 	erb:game
+ end
