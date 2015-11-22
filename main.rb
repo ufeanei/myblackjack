b6c60c861befaffccb397eace4c064337d85cd27
ufeanei <akaranjoh@yahoo.com>
2015-11-22 05:50:47 +0100

    added code to view cards on the game page. also added helpers for viewing and for calculating total

--- main.rb
+++ main.rb
@@ -4,7 +4,55 @@ require 'sinatra'
 set :sessions, true
 
 POCKET = 1000
+helpers do
+  def calculate_total(cards)
+    arr = cards.map { |e| e[1] }
 
+      total = 0
+      arr.each do |value|
+        if value == "A"
+          total += 11
+        elsif value.to_i == 0
+        total += 10
+        else
+          total += value.to_i
+        end
+      end
+  # correct for many aces
+    arr.select{|e| e == "A"}.count.times do
+      break if total <= 21
+        total -= 10
+      end
+  
+    total
+  end
+
+  def card_image(card)
+    suit = case card[0]
+    when 'H' then 'hearts'
+    when 'D' then 'diamonds'
+    when 'C' then 'clubs'
+    when 'S' then 'spades'
+  end 
+
+  value = card[1]
+  if ['J','Q', 'K', 'A'].include?(value)
+    value = case card[1]
+      when 'J' then 'jack'
+      when 'Q' then 'queen'
+      when 'K' then 'king'
+      when 'A' then 'ace'
+    end
+  end
+
+"<img src='/images/cards/#{suit}_#{value}.jpg'>"
+end
+
+end
+
+before do
+  @show_hit_or_stay_buttons = true
+end 
 
 get "/" do
   erb:template
@@ -13,7 +61,7 @@ end
 post "/name" do
   if params[:name] == ""
   	@error = "Please name can't be empty"
-  	redirect "/"
+  	erb:template
   else
     session[:name] = params[:name]
     redirect "/newbet"
@@ -29,11 +77,13 @@ end
 post "/bet" do
   
   if params[:bet].to_i > POCKET
-    redirect "/newbet"
-  	@error = "Your bet must be lower than your pocket"
+    @error = "Your bet must be lower than your pocket"
+    erb:bet
+  	
   elsif params[:bet] == ""
-  	 redirect "/newbet"	
-  	@error = "Please fill in your bet"
+    @error = "Please fill in your bet"
+  	 erb:bet	
+  	
   else
     session[:bet] = params[:bet]
     redirect "/game"
@@ -41,5 +91,31 @@ post "/bet" do
 end
  
 get "/game" do
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3','4', '5', '6', '7', '8','9', '10', 'A', 'J', 'Q', 'K', ]
+  # here we store the deck of cards in a session. Note that we have it shuffle ready to be dealt
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
   erb:game
+end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  if calculate_total(session[:player_cards]) > 21
+    @error = "Sorry You are busted."
+    @show_hit_or_stay_buttons = false
+  end
+  erb:game
+end
+
+post '/game/player/stay' do
+  @success = "you have chosen to stay"
+  @show_hit_or_stay_buttons = false
+  erb :game
 end
